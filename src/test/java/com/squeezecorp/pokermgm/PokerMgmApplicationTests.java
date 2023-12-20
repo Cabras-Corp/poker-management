09b2f0fe5bbea5f1e8f7a928c91df5f7e4713f39
andreLake <andre.lake@hotmail.com>
2023-12-19 21:19:59 -0300

    primeiro commit

---
+++ src/test/java/com/squeezecorp/pokermgm/PokerMgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squeezecorp.pokermgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerMgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
