a36b39dfa3f90f5c6e80c764470b75239726c513
João Pedro A. Hypólito <jpazhy@gmail.com>
2024-02-05 14:45:11 -0300

    feat-15-adiciona-delete-put
    Finaliza o CRUD para a entidade Player, adicionando os métodos delete e put (update)

--- src/main/java/com/squeezecorp/pokermgm/controllers/PlayerController.java
+++ src/main/java/com/squeezecorp/pokermgm/controllers/PlayerController.java
@@ -2,6 +2,7 @@ package com.squeezecorp.pokermgm.controllers;
 
 import com.squeezecorp.pokermgm.model.PlayerModel;
 import com.squeezecorp.pokermgm.repository.PlayerRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,24 +14,58 @@ import java.util.Optional;
 @RequestMapping("player")
 public class PlayerController {
 
-    // Ponto de injeção
     @Autowired
-    private PlayerRepository playerRepository; // Instanciando
-
-    public ResponseEntity<String> getTest() {
-        return new ResponseEntity<java.lang.String>("Teste", HttpStatus.OK);
-    }
+    private PlayerRepository playerRepository;
 
     @PostMapping("create")
-    public ResponseEntity<PlayerModel> createPlayer() {
-        playerRepository.save(new PlayerModel());
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Void> createPlayer(String username, String email, String password) {
+        playerRepository.save(new PlayerModel(username, email, password));
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<PlayerModel> findPlayerById(@PathVariable Long id) {
         Optional<PlayerModel> response = playerRepository.findById(id);
-        return new ResponseEntity<>(response.get(), HttpStatus.OK);
+        return response.map(player -> new ResponseEntity<>(player, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @DeleteMapping({"{id}")
+    public ResponseEntity<Void> deletePlayer(@PathVariable Long id) {
+        Optional<PlayerModel> response = playerRepository.findById(id);
+        if (response.isPresent()) {
+            playerRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("{id}")
+    @Transactional
+    public ResponseEntity<Void> updatePlayer(@PathVariable("id") Long id, @RequestBody PlayerModel updatedPlayer) {
+        Optional<PlayerModel> response = playerRepository.findById(id);
+
+        if (response.isPresent()) {
+            PlayerModel existingPlayer = response.get();
+
+            if (updatedPlayer.getUsername() != null) {
+                existingPlayer.setUsername(updatedPlayer.getUsername());
+            }
+
+            if (updatedPlayer.getEmail() != null) {
+                existingPlayer.setEmail(updatedPlayer.getEmail());
+            }
+
+            if (updatedPlayer.getPassword() != null) {
+                existingPlayer.setPassword(updatedPlayer.getPassword());
+            }
+
+            playerRepository.save(existingPlayer);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/squeezecorp/pokermgm/model/PlayerModel.java
+++ src/main/java/com/squeezecorp/pokermgm/model/PlayerModel.java
@@ -1,18 +1,17 @@
 package com.squeezecorp.pokermgm.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Table(name = "TB_PLAYER")
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlayerModel {
-    public static final String TB_PLAYER = "Player";
-
-    @Id // mapeia o id
-    @GeneratedValue(strategy = GenerationType.AUTO) // Vai gerar um id automaticamente
-
-    // Colunas da tabela TB_PLAYER
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", unique = true)
     private Long id;
 
@@ -25,15 +24,9 @@ public class PlayerModel {
     @Column(name = "password", length = 50, nullable = false)
     private String password;
 
-    // Constructors
-    public PlayerModel() {
-    }
-
-    public PlayerModel(Long id, String username, String email, String password) {
-        this.id = id;
+    public PlayerModel(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
-
 }
